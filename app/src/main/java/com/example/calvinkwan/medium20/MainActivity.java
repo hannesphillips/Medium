94869fd8dfa655ce4e366b59c583c67c22b89f63
hkwan003 <calvinhkwan@hotmail.com>
2018-02-08 13:51:13 -0800

    added new files for icons

--- app/src/main/java/com/example/calvinkwan/medium20/MainActivity.java
+++ app/src/main/java/com/example/calvinkwan/medium20/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.calvinkwan.medium20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mSendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mSendData = (Button) findViewById(R.id.addString);
     }
 }
