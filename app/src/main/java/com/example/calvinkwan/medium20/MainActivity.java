785dd72976940e8c2133c27b7a15bc04fb55627a
hkwan003 <calvinhkwan@hotmail.com>
2018-02-08 14:09:24 -0800

    added more images for the picture upload bar and new menu items for making new posts

--- app/src/main/java/com/example/calvinkwan/medium20/MainActivity.java
+++ app/src/main/java/com/example/calvinkwan/medium20/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.calvinkwan.medium20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mSendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
-        mSendData = (Button) findViewById(R.id.addString);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.action_add)
+        {
+            startActivity(new Intent(MainActivity.this, PostActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
