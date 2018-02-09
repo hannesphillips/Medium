70cdee91558f61e3dec1dacab167729259c16154
hkwan003 <calvinhkwan@hotmail.com>
2018-02-08 16:06:21 -0800

    added imagepicker

--- app/src/main/java/com/example/calvinkwan/medium20/PostActivity.java
+++ app/src/main/java/com/example/calvinkwan/medium20/PostActivity.java
@@ -1,13 +1,47 @@
 package com.example.calvinkwan.medium20;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageButton selectImage;
+
+    private static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        selectImage = (ImageButton) findViewById(R.id.imageButton);
+
+        selectImage.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent galleryintent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryintent.setType("image/*");
+                startActivityForResult(galleryintent, GALLERY_REQUEST);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK)
+        {
+            Uri imageUri = data.getData();
+
+            selectImage.setImageURI(imageUri);;
+        }
     }
 }
