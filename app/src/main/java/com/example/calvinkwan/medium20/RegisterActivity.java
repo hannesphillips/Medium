410d249810c2ff3fd7c82a4b1ec756d598a359c0
hkwan003 <calvinhkwan@hotmail.com>
2018-02-09 00:03:15 -0800

    fixed signup issue, password needs to be greater than 6 characaters

--- app/src/main/java/com/example/calvinkwan/medium20/RegisterActivity.java
+++ app/src/main/java/com/example/calvinkwan/medium20/RegisterActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -58,31 +59,32 @@ public class RegisterActivity extends AppCompatActivity {
         final String name = nameField.getText().toString().trim();
         String email = emailField.getText().toString().trim();
         String password = passwordField.getText().toString().trim();
+        if (password.length() > 5) {
+            if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                progress.setMessage("Signing up..");
+                progress.show();
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String user_id = auth.getCurrentUser().getUid();        //retrives the unique ID generated at user account creation and stores it in user_ID
+                            DatabaseReference current_user_db = database.child(user_id);
 
-        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password))
-        {
-            progress.setMessage("Signing up..");
-            progress.show();
-            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
-            {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task)
-                {
-                    if(task.isSuccessful())
-                    {
-                        String user_id = auth.getCurrentUser().getUid();        //retrives the unique ID generated at user account creation and stores it in user_ID
-                        DatabaseReference current_user_db = database.child(user_id);
+                            current_user_db.child("name").setValue(name);
+                            current_user_db.child("image").setValue("default");
+                            progress.dismiss();
 
-                        current_user_db.child("name").setValue(name);
-                        current_user_db.child("image").setValue("default");
-                        progress.dismiss();
-
-                        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(mainIntent);
+                            Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(mainIntent);
+                        }
                     }
-                }
-            });
+                });
+            }
+        }
+        else
+        {
+            Toast.makeText(RegisterActivity.this, "Password needs to be greater than six characters", Toast.LENGTH_LONG).show();
         }
     }
 }
