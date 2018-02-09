e761dc7d132cc1c7d5eb531d6c60b1dfdf5b559e
hkwan003 <calvinhkwan@hotmail.com>
2018-02-08 23:36:02 -0800

    sign up isn't working, gonna investigate

--- app/src/main/java/com/example/calvinkwan/medium20/LoginActivity.java
+++ app/src/main/java/com/example/calvinkwan/medium20/LoginActivity.java
@@ -1,13 +1,120 @@
 package com.example.calvinkwan.medium20;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity
+{
+
+    private EditText loginEmailField;
+    private EditText loginPasswordField;
+    private Button loginButton;
+    private Button registration;
+
+    private FirebaseAuth Auth;
+
+    private DatabaseReference database;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Auth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        loginEmailField = findViewById(R.id.loginEmailField);
+        loginPasswordField = findViewById(R.id.loginPasswordField);
+        loginButton = findViewById(R.id.loginButton);
+        registration = findViewById(R.id.registrationButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                checkLogin();
+            }
+        });
+
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent RegisterIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(RegisterIntent);
+            }
+        });
+    }
+
+    private void checkLogin()
+    {
+        String email = loginEmailField.getText().toString().trim();
+        String password = loginPasswordField.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password))
+        {
+            Auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        checkUserExists();
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this, "Account Set Up Needed", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void checkUserExists()
+    {
+        final String user_id = Auth.getCurrentUser().getUid();
+
+        database.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if(dataSnapshot.hasChild(user_id))
+                {
+                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(mainIntent);
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Account Set Up Needed", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
