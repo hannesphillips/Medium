4e78e387651b50b779299030610b4e75c9b84624
hkwan003 <calvinhkwan@hotmail.com>
2018-03-11 16:25:17 -0700

    fixed scrolling view in comments and message receive, edittext scrolls up when keyboard is showing

--- app/src/main/java/com/example/calvinkwan/medium20/MessageActivity.java
+++ app/src/main/java/com/example/calvinkwan/medium20/MessageActivity.java
@@ -1,16 +1,15 @@
 package com.example.calvinkwan.medium20;
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -19,13 +18,9 @@ import android.widget.Toast;
 
 
 import com.firebase.ui.database.FirebaseListAdapter;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import org.w3c.dom.Text;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -34,6 +29,25 @@ public class MessageActivity extends AppCompatActivity {
    private FirebaseListAdapter<publicMessageChat> adapter;
    RelativeLayout activity_main;
    FloatingActionButton fab;
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu)
+   {
+       MenuInflater findMenuItems = getMenuInflater();
+       findMenuItems.inflate(R.menu.messagemenu, menu);
+       return super.onCreateOptionsMenu(menu);
+   }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionGoHome:
+                Intent aboutIntent = new Intent(MessageActivity.this, BrowserActivity.class);
+                startActivity(aboutIntent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
    @Override
    protected void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -68,9 +82,15 @@ public class MessageActivity extends AppCompatActivity {
            public void onClick(View v)
            {
                 EditText input = findViewById(R.id.input);
-                FirebaseDatabase.getInstance().getReference().push().setValue(new publicMessageChat(tempUserID,input.getText().toString(),
-                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
-                input.setText("");
+                if(input.length() > 0) {
+                    FirebaseDatabase.getInstance().getReference().push().setValue(new publicMessageChat(tempUserID, input.getText().toString(),
+                            FirebaseAuth.getInstance().getCurrentUser().getEmail()));
+                    input.setText("");
+                }
+                else
+                {
+                    Toast.makeText(MessageActivity.this, "Please enter in a message", Toast.LENGTH_LONG).show();
+                }
            }
        });
        //check if not signed in
